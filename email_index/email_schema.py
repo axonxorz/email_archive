9ef56429aa9817089b765edfc531efb770c8b456
Brendan Zerr <bzerr@brainwire.ca>
2017-07-26 10:12:19 -0600

    Improve whoosh schema, add msg_from_hit utility method

--- email_index/email_schema.py
+++ email_index/email_schema.py
@@ -6,8 +6,8 @@ from analyzers import EmailAddressAnalyzer
 
 email_schema = Schema(message_id=ID(stored=True, unique=True),
                       path=ID(stored=True, unique=True),
-                      from_addr=TEXT(stored=True, analyzer=EmailAddressAnalyzer()),
-                      to_addr=TEXT(stored=True, analyzer=EmailAddressAnalyzer()),
+                      from_addr=KEYWORD(stored=True, analyzer=EmailAddressAnalyzer()),
+                      to_addr=KEYWORD(stored=True, analyzer=EmailAddressAnalyzer()),
                       has_attachments=ID(stored=True),
                       date=DATETIME(stored=True),
                       subject=TEXT(stored=True),
--- email_index/utils.py
+++ email_index/utils.py
@@ -1,8 +1,20 @@
+import os
 import email.utils
+import email.parser
 
 import arrow
+
+from . import config
 
 
 def emaildate_to_arrow(date):
     return arrow.get(email.utils.mktime_tz(email.utils.parsedate_tz(date)))
+
+
+def msg_from_hit(hit):
+    """Return an email.Message based on the path in a given Whoosh Hit object"""
+    path = os.path.join(config.DOCUMENTS_DIR, hit['path'])
+    parser = email.parser.Parser()
+    with open(path, 'rb') as fd:
+        return parser.parse(fd)
 
